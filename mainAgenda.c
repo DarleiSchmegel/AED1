ad83f2deb0f8cd1617ecc0c683c71ecbc658d56a
Darlei Schmegel <dmschmegel@inf.ufpel.edu.br>
2019-03-25 21:53:37 -0300

    Depois da aula 25/03/19

--- mainAgenda.c
+++ mainAgenda.c
@@ -1,12 +1,40 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+struct pessoa{
+    char nome[20];
+    int teleone;
+};
+
+void insere(void *pBuffer){
+    int *quantAdd;
+    int *zero;
+    quantAdd = pBuffer;
+    zero = quantAdd++;
+
+}
 
 int main(){
 	void *pBuffer;
+	int *i;
+	int *j;
+	float *kFloat;
+
+
+
+    pBuffer = malloc((sizeof(int))+(sizeof(int))+sizeof(float));
+    i =(int*) pBuffer;
+    j = (int*)i+1;
+    kFloat = (float*) kFloat+1;
+
+    printf("Diga quantas pessoas vc deseja adicionar inicialmente: ");
+    scanf("%d",&(*i));
+    *j = 0;
+
+    printf("I = %d\n",*i);
+    printf("J = %d\n",*j);
 
 
 	free(pBuffer);
-	
+
 	return 0;
 }
