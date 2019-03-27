41830b65c82a3dd0f9e3ec4e8c6261cb515635cc
Darlei Schmegel <darleischmegel94@gmail.com>
2019-03-26 21:48:09 -0300

    Progresso do dia 26/03/19

--- mainAgenda.c
+++ mainAgenda.c
@@ -5,33 +5,68 @@ struct pessoa{
     int teleone;
 };
 
-void insere(void *pBuffer){
-    int *quantAdd;
-    int *zero;
-    quantAdd = pBuffer;
-    zero = quantAdd++;
+void insere(void *pBuffer,int *quantAdd){
+	int *quantAcumulada;
+	quantAcumulada = pBuffer;
+	
+	printf("%d\n",*quantAcumulada);
+	//printf("%d\n",quantAcumulada);
+
+	
+    
 
 }
 
 int main(){
 	void *pBuffer;
-	int *i;
-	int *j;
-	float *kFloat;
+	int *i;//pBuffer[0]
+	int *j;//pBuffer[1]
+	int *k;//pBuffer[2]
+	int *quantPessoas;//somatorio das n pessoas na agenda! pBuffer[3]
 
 
-
-    pBuffer = malloc((sizeof(int))+(sizeof(int))+sizeof(float));
+    pBuffer = malloc((sizeof(int))+(sizeof(int))+sizeof(int)+sizeof(int));
     i =(int*) pBuffer;
     j = (int*)i+1;
-    kFloat = (float*) kFloat+1;
+    k = (int*) j+1;
+    quantPessoas = (int*)k+1;
+    *quantPessoas = 0;
+	
 
-    printf("Diga quantas pessoas vc deseja adicionar inicialmente: ");
-    scanf("%d",&(*i));
-    *j = 0;
+	printf("Digite 1 para add contato ,2 para retirar contado,3 imprimir contatos,4 sair do programa! \n");
+	scanf("%d",&(*i));
+	while((*i) != 4){
+			switch(*i){
+					case 1:
+						printf("Digite quantos contatoss vc deseja add na agenda: ");
+						scanf("%d",&(*j));
+						
+						insere(pBuffer,j);
+					
+					
+					break;
+					case 2:
+					
+					break;
+					
+					case 3:
+					
+					break;
+				
+				
+			}
+		printf("Digite 1 para add contato ,2 para retirar contado,3 imprimir contatos,4 sair do programa! \n");
+		scanf("%d",&(*i));
+		
+		
+		
+		
+	}
+	
+	
 
-    printf("I = %d\n",*i);
-    printf("J = %d\n",*j);
+    //printf("I = %d\n",*i);
+    //printf("J = %d\n",*j);
 
 
 	free(pBuffer);
