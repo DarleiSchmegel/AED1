898fa4e1811cf3e7696f16923cd348e42dabc2ae
Darlei Schmegel <darleischmegel94@gmail.com>
2019-04-16 18:42:38 -0300

    Inserir,listar e remover funcionando

--- mainAgenda.c
+++ mainAgenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 25
 #define TMAX 15
 typedef struct pessoa{
@@ -17,21 +18,63 @@ typedef struct variaveis{
 
 tVariaveis *var;
 tPessoa *pessoas;
+tPessoa *auxRemove;
+void* pBuffer;
 
+void insere(){
+        var->cont = var->cont+1;
+        var->tamanhoPbuffer = (sizeof(struct variaveis)) + (var->cont * sizeof(struct pessoa));
+        pBuffer = realloc(pBuffer,var->tamanhoPbuffer);
 
-void insere(struct pessoa* pessoas){
+        var = pBuffer;
+        pessoas = pBuffer+(var->tamanhoPbuffer - sizeof(struct pessoa));
+
 		getchar();
+
         printf("Digite os dados a seguir!\n");
         printf("Nome : ");
         fgets(pessoas->nome, MAX, stdin);
-        
+
         printf("Telefone: ");
         fgets(pessoas->telefone, TMAX, stdin);
 }
-void deleta(struct pessoa *lista,char* rNome,int* i,int *cont){
-	for (*i = 0; *i < *cont; (*i)++){
-		printf("pessoa[%d] = %s\n",*i,lista->nome);
+void deleta(){
+	printf("Digite o nome do contato que vc deseja excluir: ");
+	getchar();
+	fgets(var->auxNome, MAX, stdin);
+	//printf("%s",var->auxNome);
+	var->i = 0;
+	pessoas = pBuffer + (sizeof(struct variaveis));
+	while(strcmp(var->auxNome,pessoas->nome) != 0 && var->i < var->cont){
+
+        pessoas = (tPessoa*)pessoas+1 ;
+        var->i = var->i + 1;
+
 	}
+    if(var->i == var->cont){
+    printf("Este nome nao esta na agenda!\n ");
+    }
+    else{
+        auxRemove = (tPessoa*)pessoas+1;
+
+        while(var->i < var->cont){
+            strcpy(pessoas->nome,auxRemove->nome);
+            strcpy(pessoas->telefone,auxRemove->telefone);
+
+            auxRemove = (tPessoa*)auxRemove+1;
+            pessoas = (tPessoa*)pessoas+1;
+            var->i = var->i+1;
+        }
+        var->cont = var->cont -1;
+        var->tamanhoPbuffer = (sizeof(struct variaveis)) + (var->cont * sizeof(struct pessoa));
+        pessoas = pBuffer+(var->tamanhoPbuffer - sizeof(struct pessoa));
+       /* if(!pessoas){
+            return -1;
+        }*/
+        printf("Contato removido com sucesso!\n");
+    }
+
+
 }
 
 struct pessoa *retornaLugar(struct pessoa* pessoas,char *rNome,int* i,int* cont){
@@ -43,8 +86,8 @@ struct pessoa *retornaLugar(struct pessoa* pessoas,char *rNome,int* i,int* cont)
 }
 
 int main(){
-    void* pBuffer;
-	
+    //void* pBuffer;
+
 
     pBuffer = malloc(sizeof(struct variaveis));
 	var = pBuffer;
@@ -56,26 +99,11 @@ int main(){
 	while(var->menu != 4){
 			switch(var->menu){
 					case 1:
-						var->cont = var->cont+1;
-						var->tamanhoPbuffer = (sizeof(struct variaveis)) + (var->cont * sizeof(struct pessoa));
-
-           			 	pBuffer = realloc(pBuffer,var->tamanhoPbuffer);
-
-           			 	var = pBuffer;
-           			 	pessoas = pBuffer+(var->tamanhoPbuffer - sizeof(struct pessoa));
-
-            			insere(pessoas);
+                        insere();
 
 					break;
 					case 2:
-
-						/*printf("Digite o nome que vc deseja remover: \n");
-						scanf("%s",xNome);
-						pessoas = (pessoas,xNome,i,cont);
-						//deleta(pessoas,i,cont);
-						if(pessoas == NULL) printf("Este contato nao esta na agenda!!!\n"); 
-						pessoas =(char*)xNome+(sizeof(char)*20);*/
-						
+                        deleta();
 					break;
 
 
