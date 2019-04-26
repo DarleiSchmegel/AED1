e7c304b56fdd5cf8c3c20b9555e8fd33c397122e
Darlei Schmegel <darleischmegel94@gmail.com>
2019-04-25 21:48:25 -0300

    InsertionSort Funcionando

--- mainAgenda.c
+++ mainAgenda.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define MAX 25
 #define TMAX 15
+#define OFFSET 97
 typedef struct pessoa{
     char nome[MAX];
     char telefone[TMAX];
@@ -10,16 +11,45 @@ typedef struct pessoa{
 typedef struct variaveis{
 	int menu;
 	int i;
+	int j;
 	int cont;
 	int tamanhoPbuffer;
 	char auxNome[MAX];
+	char auxTelefone[TMAX];
+	tPessoa *auxRemove;
 }tVariaveis;
-
 
 tVariaveis *var;
 tPessoa *pessoas;
-tPessoa *auxRemove;
 void* pBuffer;
+
+void nomesIniciais(){
+	preenche();
+	strcpy(pessoas->nome,"Darlei\n");
+	strcpy(pessoas->telefone,"11111\n");
+
+	preenche();
+	strcpy(pessoas->nome,"Ana\n");
+	strcpy(pessoas->telefone,"222222\n");
+
+	preenche();
+	strcpy(pessoas->nome,"Paula\n");
+	strcpy(pessoas->telefone,"333333\n");
+
+	preenche();
+	strcpy(pessoas->nome,"Bruna\n");
+	strcpy(pessoas->telefone,"44444\n");
+
+
+}
+void preenche(){
+		var->cont = var->cont+1;
+        var->tamanhoPbuffer = (sizeof(struct variaveis)) + (var->cont * sizeof(struct pessoa));
+        pBuffer = realloc(pBuffer,var->tamanhoPbuffer);
+
+        var = pBuffer;
+        pessoas = pBuffer+(var->tamanhoPbuffer - sizeof(struct pessoa));
+}
 
 void insere(){
         var->cont = var->cont+1;
@@ -55,13 +85,13 @@ void deleta(){
     printf("Este nome nao esta na agenda!\n ");
     }
     else{
-        auxRemove = (tPessoa*)pessoas+1;
+        var->auxRemove = (tPessoa*)pessoas+1;
 
         while(var->i < var->cont){
-            strcpy(pessoas->nome,auxRemove->nome);
-            strcpy(pessoas->telefone,auxRemove->telefone);
+            strcpy(pessoas->nome,var->auxRemove->nome);
+            strcpy(pessoas->telefone,var->auxRemove->telefone);
 
-            auxRemove = (tPessoa*)auxRemove+1;
+            var->auxRemove = (tPessoa*)var->auxRemove+1;
             pessoas = (tPessoa*)pessoas+1;
             var->i = var->i+1;
         }
@@ -76,13 +106,39 @@ void deleta(){
 
 
 }
-
-struct pessoa *retornaLugar(struct pessoa* pessoas,char *rNome,int* i,int* cont){
-	for(*i = 0; *i < *cont;(*i)++){
-		if(pessoas->nome == rNome) return pessoas;
-		pessoas = pessoas+1;
+void listar(){
+	printf("Listando...\n");
+	pessoas = pBuffer + (sizeof(struct variaveis));
+	for(var->i = 0;var->i < var->cont; (var->i)++ ){
+		printf("===========================\nContato[%d]\n",var->i);
+		printf("Nome   = %s",pessoas[var->i].nome);
+		printf("Numero = %s",pessoas[var->i].telefone);
+		printf("===========================\n");
+		//pessoas = pBuffer + (sizeof(struct variaveis) + ((var->i) * sizeof(struct pessoa)));
 	}
-	return NULL;
+}
+void insertionSort(){
+
+	pessoas = pBuffer + (sizeof(struct variaveis) );
+	for (var->j = 1; var->j < var->cont; (var->j)++) {
+
+    	var->i = var->j - 1;
+    	strcpy(var->auxNome , pessoas[var->j].nome);
+		strcpy(var->auxTelefone , pessoas[var->j].telefone);
+
+		//strcmp(str1, str2) > 0 ent? str1 > str2 se strcmp(str1, str2) < 0 ent? str1 < str2 se strcmp(str1, str2) == 0 ent? str1 
+		//== str2   O que tamb? implica que: se strcmp(str1, str2) >= 0 ent? str1 >=
+    	while ( (var->i >= 0) && ((strcmp(var->auxNome ,pessoas[var->i].nome)) < 0 )) { //while ( (i>=0) && (tmp < data[i]) )
+		
+        	strcpy(pessoas[var->i +1].nome,pessoas[var->i].nome);
+			strcpy(pessoas[var->i +1].telefone,pessoas[var->i].telefone);
+
+        	(var->i)--;
+
+    	}
+    	strcpy(pessoas[var->i +1].nome ,var->auxNome );
+    	strcpy(pessoas[var->i +1].telefone,var->auxTelefone);
+	}
 }
 
 int main(){
@@ -92,35 +148,26 @@ int main(){
     pBuffer = malloc(sizeof(struct variaveis));
 	var = pBuffer;
 	var->cont = 0;
+	nomesIniciais();
 
-    printf("\nDigite! \n1 para add contato ,\n2 para retirar contado,\n3 imprimir contatos,\n4 sair do programa! \n\n");
+    printf("\nDigite! \n1 para add contato ,\n2 para retirar contado,\n3 imprimir contatos,\n4 sair do programa! \n5 ordenar\n\n");
     scanf("%d",&var->menu);
 
 	while(var->menu != 4){
 			switch(var->menu){
 					case 1:
                         insere();
-
 					break;
 					case 2:
                         deleta();
 					break;
-
-
 					case 3:
-						printf("Listando...\n");
-						pessoas = pBuffer + (sizeof(struct variaveis));
-
-						for(var->i = 1;var->i <= var->cont; (var->i)++ ){
-							printf("===========================\nContato[%d]\n",var->i);
-							printf("Nome   = %s",pessoas->nome);
-							printf("Numero = %s",pessoas->telefone);
-							printf("===========================\n");
-							pessoas = pBuffer + (sizeof(struct variaveis) + ((var->i) * sizeof(struct pessoa)));
-						}
+						listar();					
+					case 5:
+						insertionSort();
 					break;
 			}
-		printf("\nDigite! \n1 para add contato ,\n2 para retirar contado,\n3 imprimir contatos,\n4 sair do programa! \n");
+		printf("\nDigite! \n1 para add contato ,\n2 para retirar contado,\n3 imprimir contatos,\n4 sair do programa! \n5 ordenar\n");
 		scanf("%d",&var->menu);
 	}
 	free(pBuffer);
